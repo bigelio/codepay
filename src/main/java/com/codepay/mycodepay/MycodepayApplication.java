314eee54f90bffb131431f22baf3d4f83d0f28c9
Gelio BIZIMANA <70667354+bigelio@users.noreply.github.com>
2022-09-28 21:11:59 +0200

    fix: resolve conflicts and clean up

---
+++ src/main/java/com/codepay/mycodepay/MycodepayApplication.java
@@ -0,0 +1,13 @@
+package com.codepay.mycodepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycodepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycodepayApplication.class, args);
+	}
+
+}
